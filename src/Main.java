cf250cdcb527217e5fa633d1b972daa8d2c9fcee
Igoc <zx0342@gmail.com>
2022-07-11 10:02:05 +0900

    Update 2022-07-07

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
         // 커피 주문 -> 커피 획득
         Item coffee = clerk.order("Coffee", coffeePrice);
         System.out.println("Name = " + coffee.getName() + ", Price = " + coffee.getPrice() + ", Shelf Life = " + coffee.getShelfLife());
+        coffee.showTakeMethod();
 
         // 커피 주문 (돈 부족) -> 예외 발생
         try {
@@ -19,6 +20,15 @@ public class Main {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
+        // 케이크 가격 요청 -> 케이크 가격 획득
+        int cakePrice = clerk.getPrice("Cake");
+        System.out.println("Cake Price = " + cakePrice);
+
+        // 케이크 주문 -> 케이크 획득
+        Item cake = clerk.order("Cake", cakePrice);
+        System.out.println("Name = " + cake.getName() + ", Price = " + cake.getPrice() + ", Shelf Life = " + cake.getShelfLife());
+        cake.showTakeMethod();
 
         // 스무디 가격 요청 (판매하지 않는 메뉴) -> 예외 발생
         try {
